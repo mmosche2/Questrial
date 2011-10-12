dfc43c90f4f9c4e2bee0612a10f3ed6a7b12a043
Mark Moschel <mark.moschel@gmail.com>
2011-10-12 01:32:24 -0500

    User signup complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,11 @@ class UsersController < ApplicationController
   def create  
     @user = User.new(params[:user])  
     if @user.save  
-      redirect_to user_path(@user), :notice => "Signed up!"  
+	  flash[:success] = "Welcome to Questrial!"
+      redirect_to @user 
     else  
-      render "new"  
+      @title = "Sign up"
+	  render 'new'
     end  
   end  
   
