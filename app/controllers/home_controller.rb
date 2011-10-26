b6ec1e4a6de25d73c79a330b9059e1f8dc196aa1
Mark Moschel <mark.moschel@gmail.com>
2011-10-25 23:22:23 -0400

    updated home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+	@quests = Quest.where("start >= ?", Date.today).order("start ASC").limit(2)
   end
 
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,8 +22,8 @@ Questrial::Application.configure do
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
 
-  # Do not compress assets
-  config.assets.compress = false
+  # Do not compress assets -- 10/22 MODIFIED TO TRUE ***************
+  config.assets.compress = true
 
   # Expands the lines which load the assets
   config.assets.debug = true
