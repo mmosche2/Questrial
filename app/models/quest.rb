2bad0d957cdb9daf87bb50088c0af6cbe01e0424
Mark Moschel <mark.moschel@gmail.com>
2011-11-12 04:56:04 -0500

    added gif

--- app/models/quest.rb
+++ app/models/quest.rb
@@ -26,7 +26,7 @@ class Quest < ActiveRecord::Base
 
 				  
 	validates_attachment_size :photo, :less_than => 5.megabytes
-	validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png']	
+	validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png', 'image/gif']	
 	
 
 	
