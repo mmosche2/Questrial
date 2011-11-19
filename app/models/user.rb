4c2794a12b5c3698d211645077a31dbab5d78a9b
Mark Moschel <mark.moschel@gmail.com>
2011-11-18 19:36:30 -0500

    admin work

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-	attr_accessible :email, :password, :password_confirmation, :name
+	attr_accessible :email, :password, :password_confirmation, :name, :admin
 	has_secure_password
 	validates_presence_of :password, :on => :create
 	
