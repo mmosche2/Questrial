3ac6b57880ef2cfadc04adc0a32b9093a6825170
Mark Moschel <mark.moschel@gmail.com>
2011-11-18 22:47:52 -0500

    removing admin option

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-	attr_accessible :email, :password, :password_confirmation, :name, :admin
+	attr_accessible :email, :password, :password_confirmation, :name
 	has_secure_password
 	validates_presence_of :password, :on => :create
 	
