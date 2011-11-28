3e54b1ab683e66ce968f25985efe873bd32d28df
Mark Moschel <mark.moschel@gmail.com>
2011-11-27 19:56:16 -0500

    undoing the migration fix

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -2,7 +2,7 @@ class Experience < ActiveRecord::Base
 	attr_accessible :joined_id, :startdate, :enddate
 	
 	belongs_to :joiner, :class_name => "User"
-	belongs_to :joined, :class_name => "Quest"
+	belongs_to :joined, :class_name => "Quest", :counter_cache => :joiners_count
 	
 	validates :joiner_id, :presence => true
 	validates :joined_id, :presence => true
